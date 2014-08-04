bcdd62cc730eb8da145ed31f5c07787e13511da0
Ben Irvin <ben@write-together.com>
2014-08-04 13:15:09 +0000

    fix install

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,17 +3,13 @@
 var fs = require('fs');
 var path = require('path');
 
-var configDir   = path.normalize(__dirname + '/../config');
 var localFile   = path.normalize(__dirname + '/../config/autosession.js');
-var projectFile = path.normalize(__dirname + '/../config/plugins/ah-autosession-plugin.js');
+var projectFile = path.normalize(process.cwd() + '/../../config/plugins/ah-autosession-plugin.js');
 
-// create config/plugins directory if it doesn't exist
-if(!fs.existsSync(configDir)) {
-  fs.mkdirSync(__dirname + '/../config/plugins');
+if(!fs.existsSync(process.cwd() + '/../../config/plugins')) {
+  fs.mkdirSync(process.cwd() + '/../../config/plugins');
 }
-
-// copy default config file if it doesn't exist
-if(!fs.existsSync(projectFile) && fs.existsSync(localFile)) {
+if(!fs.existsSync(projectFile)){
   console.log("copying " + localFile + " to " + projectFile);
   fs.createReadStream(localFile).pipe(fs.createWriteStream(projectFile));
 }
