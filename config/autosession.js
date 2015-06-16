8e770df1b2f2a36cbd494faeed79d5c6f94dffa2
Ben Irvin <ben@write-together.com>
2015-06-16 16:20:04 +0200

    update for actionhero v11. no longer compatible with lower versions.

--- config/autosession.js
+++ config/autosession.js
@@ -1,24 +1,27 @@
 exports.default = {
   autosession: function(api){
     return {
-      default: true, // true=load but don't require, false=don't autoload (disable plugin), 'required'=session token required to run action
-      connection_param: "session", // where will the session data be available in connection object? connection.session
+      name: 'autosession', // middleware name
+      priority: 20, // middleware priority
+      global: true, // middleware global value. if false, must manually specify on each page
+      default_require: false, // true=prevent action if session missing, false=not required (but loaded if exists)
+      global_override_key: "autosession", // if an action doesn't require it, like login or public page, set action.autosession = false
+      connection_param: "session", // where will the session data be available in data object? data.session
       app_id: "sess", // app id used by redis_sessions
       valid: { // custom regex validators used by redis_sessions
-        app: /^([a-zA-Z0-9_-]){1,20}$/, // just make sure it allows app_id set above
-        id: /^([a-zA-Z0-9_-]){1,128}$/, // this should match your user ids
-        token: /^([a-zA-Z0-9]){64}$/, // don't change this unless you know what you're doing
+        app: /^([a-zA-Z0-9_-]){1,20}$/, // make sure it allows app_id set above
+        id: /^([a-zA-Z0-9_-]){1,128}$/, // this should validate your user ids
+        token: /^([a-zA-Z0-9]){64}$/, // validates token (don't change unless you know what you're doing)
         ip: /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$|^unknown$|^testServer$/, // allows ip address, "unknown", or "testServer"
         //ip: /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/, // use this if you want to throw error on invalid/missing ip
       },
       prefix: 'ah:sess:', // redis prefix
       ttl:1000 * 60 * 60 * 24 * 14, // session time to live; 1000 * 60 * 60 * 24 * 14 = two weeks
-      token_param: "t", // this is the name of the token parameter added to all actions that use autosession
+      token_param: "t", // name of the token parameter added to all actions that use autosession
       error: {
         required: "A session token is required to perform this action",
         invalid: "Your session token is invalid",
       },
-      priority: 3, // action preProcessor priority
     };
   }
 };
--- initializers/autosession.js
+++ initializers/autosession.js
@@ -1,133 +1,137 @@
-var autosession = function(api, next){
-  if(!api.config.autosession) return next();
-  
-  var a, i, j, redis = api.redis.client, RedisSessions = require("redis-sessions");
-  
-  api.autosession = {};
-  
-  ////// Public functions (mostly wrappers for redissessions)
-  api.autosession.create = function(user_id, options, cb) {
-    if(!options) options = {};
-    if(!options.ip) options.ip = "unknown";
-    if(!options.ttl) options.ttl = api.config.autosession.ttl;
-    api.autosession.rs.create(api.autosession.idkey(user_id, options.ip, options.ttl), cb);
-  };
-  api.autosession.kill = function(token, cb) {
-    api.autosession.rs.kill(api.autosession.tkey(token), cb);
-  };
-  api.autosession.killAll = function(user_id, cb) {
-    api.autosession.rs.killsoid(api.autosession.idkey(user_id), cb);
-  };
-  api.autosession.set = function(token, data, cb) {
-    var s = api.autosession.tkey(token);
-    s.d = data;
-    api.autosession.rs.set(s, cb);
-  };
-  api.autosession.get = function(token, cb) {
-    api.autosession.rs.get(api.autosession.tkey(token), cb);
-  };
-  
-  ////// Private functions (ok, actually undocumented, since most are intentionally left publicly accessible)
-  
-  // helper functions to build rs objects
-  api.autosession.tkey = function(token) {
-    return {app:api.config.autosession.app_id, token:token};
-  };
-  api.autosession.idkey = function(id, ip, ttl) {
-    return {app:api.config.autosession.app_id,
-            id:id,
-            ip:(ip ? ip : ""),
-            ttl:(ttl ? ttl : api.config.autosession.ttl),
-    };
-  };
-  
-  // check if actionTemplate requires our session parameter
-  api.autosession._required = function(actionTemplate) {
-    return (actionTemplate.inputs.required.indexOf(api.config.autosession.token_param) !== -1);
-  };
-  
-  // check if actionTemplate allows our session parameter
-  api.autosession._optional = function(actionTemplate) {
-    return (actionTemplate.inputs.optional.indexOf(api.config.autosession.token_param) !== -1);
-  };
-  
-  ////// Run setup code
-  
-  // set up rs object with RedisSessions
-  api.autosession.rs = new RedisSessions({namespace:api.config.autosession.prefix});
-  api.autosession.rs._VALID = api.config.autosession.valid; // replace validator regex array
-
-  // add token parameter as optional/required as necessary to each action
-  function input_exists(a) {
-    return (
-      a.inputs.optional.indexOf(api.config.autosession.token_param) !== -1 ||
-      a.inputs.required.indexOf(api.config.autosession.token_param) !== -1
-    );
-  }
-  for(i in api.actions.actions) {
-    for(j in api.actions.actions[i]) {
-      a = api.actions.actions[i][j];
-      // if this action doesn't have an explicit action set, use the default
-      if(a.autosession === null || a.autosession === undefined) {
-        //api.log("setting autosession for action "+a.name+" to default: "+api.config.autosession.default);
-        a.autosession = api.config.autosession.default;
-      }
-      
-      // and now check for real:
-      
-      // don't load session
-      if(a.autosession === false) {
-        //api.log("setting autosession for action "+a.name+" to false");
-         // nothing extra to do
-      }
-      else if(input_exists(a)) {
-        //api.log('Tried adding session token parameter "'+api.config.autosession.token_param+'" to action "'+a.name+'" but it already exists','warning');
-      }
-      // load if available, but not required
-      else if(a.autosession === true) {
-        //api.log("setting autosession for action "+a.name+" to true");
-        a.inputs.optional.push(api.config.autosession.token_param);
-      }
-      // session data required to continue
-      else if(a.autosession === "required" ) {
-        //api.log("setting autosession for action "+a.name+" to required");
-        a.inputs.required.push(api.config.autosession.token_param);
-      }
-    }
-  }
-  
-  // then rebuild the post variables or so our required params actually get passed in
-  api.params.buildPostVariables();
-  
-  // add the preprocessor middleware
-  api.autosession._on_action = function(connection, actionTemplate, cb) {
-    var token,
-        required = api.autosession._required(actionTemplate),
-        optional = api.autosession._optional(actionTemplate);
+module.exports = {
+  loadPriority:  50,
+  startPriority: 50,
+  stopPriority:  50,
+  initialize: function(api, next) {
+    next();
+  },
+  stop: function(api, next) {
+    next();
+  },
+  start: function(api, next){
+    if(!api.config.autosession) return next("missing api.config.autosession");
     
-    // if it's required or (optional and token was sent)
-    if(required || (optional && connection.params && connection.params[api.config.autosession.token_param])) {
-      token = connection.params[api.config.autosession.token_param];
-      if(!token || !token.length) {
-        connection.error = api.config.autosession.error.required;
-        return cb(connection, false);
+    var a, i, j, redis = api.redis.client, RedisSessions = require("redis-sessions");
+    
+    api.autosession = {};
+    
+    ////// Public functions (mostly wrappers for redissessions)
+    api.autosession.create = function(user_id, options, cb) {
+      if(!options) options = {};
+      if(!options.ip) options.ip = "unknown";
+      if(!options.ttl) options.ttl = api.config.autosession.ttl;
+      api.autosession.rs.create(api.autosession.idkey(user_id, options.ip, options.ttl), cb);
+    };
+    api.autosession.kill = function(token, cb) {
+      api.autosession.rs.kill(api.autosession.tkey(token), cb);
+    };
+    api.autosession.killAll = function(user_id, cb) {
+      api.autosession.rs.killsoid(api.autosession.idkey(user_id), cb);
+    };
+    api.autosession.set = function(token, data, cb) {
+      var s = api.autosession.tkey(token);
+      s.d = data;
+      api.autosession.rs.set(s, cb);
+    };
+    api.autosession.get = function(token, cb) {
+      api.autosession.rs.get(api.autosession.tkey(token), cb);
+    };
+    
+    ////// Private functions (ok, actually undocumented, since most are intentionally left publicly accessible)
+    
+    // helper functions to build rs objects
+    api.autosession.tkey = function(token) {
+      return {app:api.config.autosession.app_id, token:token};
+    };
+    api.autosession.idkey = function(id, ip, ttl) {
+      return {app:api.config.autosession.app_id,
+              id:id,
+              ip:(ip ? ip : ""),
+              ttl:(ttl ? ttl : api.config.autosession.ttl),
+      };
+    };
+    
+    // check if actionTemplate requires our session parameter
+    api.autosession._required = function(actionTemplate) {
+      if(actionTemplate[api.config.autosession.global_override_key]) return actionTemplate[api.config.autosession.global_override_key];
+      
+      if(actionTemplate.inputs[api.config.autosession.token_param]) {
+        if(actionTemplate.inputs[api.config.autosession.token_param].required === true) return true;
+        return false;
       }
-      api.autosession.get(token, function(err, res) {
-        if(err) connection.error = api.e(err);
-        else if(!res || !res.id) connection.error = api.config.autosession.error.invalid;
-        else connection[api.config.autosession.connection_param] = res;
+      
+      
+      return api.config.autosession.default_require;
+    };
+    
+    ////// Run setup code
+    
+    // set up rs object with RedisSessions
+    api.autosession.rs = new RedisSessions({namespace:api.config.autosession.prefix});
+    api.autosession.rs._VALID = api.config.autosession.valid; // replace validator regex array
+
+    // add token parameter as optional/required as necessary to each action
+    function input_exists(a) {
+      return !!a.inputs[api.config.autosession.token_param];
+    }
+    for(i in api.actions.actions) { // i = action name (ie, "status")
+      for(j in api.actions.actions[i]) { // j = 
+        a = api.actions.actions[i][j];
+        // if this action doesn't have an explicit action set, use the default
+        if(a.autosession === null || a.autosession === undefined) {
+          //api.log("setting autosession for action "+a.name+" to default: "+api.config.autosession.default);
+          a.autosession = api.config.autosession.default_require;
+        }
         
-        return cb(connection, !connection.error);
+        // and now check for real:
+        if(input_exists(a)) {
+          // don't do anything
+          //api.log('Tried adding session token parameter "'+api.config.autosession.token_param+'" to action "'+a.name+'" but it already exists','warning');
+        }
+        // load if available, but not required
+        else if(a.autosession === false) {
+          //api.log("setting autosession for action "+a.name+" to true");
+          a.inputs[api.config.autosession.token_param] = {required:false};
+        }
+        // session data required to continue
+        else if(a.autosession === true ) {
+          //api.log("setting autosession for action "+a.name+" to required");
+          a.inputs[api.config.autosession.token_param] = {required:true};
+        }
+      }
+    }
+    
+    // then rebuild the post variables or so our required params actually get passed in
+    api.params.buildPostVariables();
+    
+    // add the preprocessor middleware
+    api.autosession._on_action = function(data, mid_next) {
+      var token,
+          actionTemplate = data.actionTemplate,
+          required = api.autosession._required(actionTemplate);
+      
+      // if it's required or (optional and token was sent)
+      token = data.params[api.config.autosession.token_param];
+      if(!token) {
+        if(required) return mid_next(api.config.autosession.error.required);
+        return mid_next();
+      }
+    
+      api.autosession.get(token, function(err, res) {
+        if(err) return mid_next(err);
+        if(!res || !res.id) return mid_next(api.config.autosession.error.invalid);
+        data[api.config.autosession.connection_param] = res;
+        return mid_next();
       });
+    };
+    var middleware = {
+      name: api.config.autosession.name,
+      global: api.config.autosession.global,
+      priority: api.config.autosession.priority,
+      preProcessor: api.autosession._on_action,
     }
-    else { // if this item doesn't automatically look up a token
-      return cb(connection, true);
-    }
-  };
-  
-  if(require('semver').lt(require('../../actionhero/package.json').version, '9.0.0')) api.actions.preProcessors.push(api.autosession._on_action);
-  else api.actions.addPreProcessor(api.autosession._on_action, (api.config.autosession.priority ? api.config.autosession.priority : 3));
-  
-  next();
-};
-exports.autosession = autosession;
+    api.actions.addMiddleware(middleware);
+    
+    next();
+  },
+}
